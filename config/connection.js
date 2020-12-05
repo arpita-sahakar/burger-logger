aecdd7d67e49354a9f97bebf01a9ce34060a3273
Arpita Saha <arpita.sahaa@gmail.com>
2020-12-04 20:44:04 -0500

    Changed database properties with Heroku DB

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,22 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "root",
+//   database: "burgers_db"
+// });
 
+
+const connection = mysql.createConnection({
+  host: "ixnzh1cxch6rtdrx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  port: 3306,
+  user: "kqlpahnoeubbalj5",
+  password: "muu3d2eftcek44s7",
+  database: "yucg4x3tmv9t38gp"
+});
 // Make connection.
 connection.connect(function(err) {
   if (err) {
